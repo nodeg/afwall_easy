6fae8a16675b79a8850bc077025471ec7f27e8ad
mglinux <mglinux@cryptolab.net>
2016-07-31 15:41:54 +0200

    Signed-off-by: mglinux <mglinux@cryptolab.net>

--- afwall.sh
+++ afwall.sh
@@ -118,6 +118,64 @@ break
 esac
 done
 clear
+echo "Block Apple? y/n"
+echo -n ":"
+while read Option
+do
+case $Option in
+y|Y)
+echo Adding Apple ASN to list
+curl --silent 'https://stat.ripe.net/data/announced-prefixes/data.json?preferred_version=1.1&resource=AS714' | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/[0-9]{1,2}' | uniq > apple.txt
+python2 \script/apple.py >> iptables_on.sh
+break
+;;
+n|N)
+echo "Skipped Apple"
+break
+;;
+esac
+done
+clear
+#Amazon list is really big and right now not applyable by afwall on some devices
+#echo "Block Amazon? y/n"
+#echo -n ":"
+#while read Option
+#do
+#case $Option in
+#y|Y)
+#echo Adding Amazon ASN to list
+#curl --silent 'https://stat.ripe.net/data/announced-prefixes/data.json?preferred_version=1.1&resource=AS16509' | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/[0-9]{1,2}' | uniq > amazon.txt
+#python2 \script/amazon.py >> iptables2_on.sh
+#break
+#;;
+#n|N)
+#echo "Skipped Amazon"
+#break
+#;;
+#esac
+#done
+clear
+echo "Block Microsoft? y/n"
+echo -n ":"
+while read Option
+do
+case $Option in
+y|Y)
+echo Adding Microsoft ASN to list
+curl --silent 'https://stat.ripe.net/data/announced-prefixes/data.json?preferred_version=1.1&resource=AS8075' | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/[0-9]{1,2}' | uniq > microsoft.txt
+python2 \script/microsoft.py >> iptables2_on.sh
+break
+;;
+n|N)
+echo "Skipped Microsoft"
+break
+;;
+esac
+done
+clear
+rm microsoft.txt
+rm amazon.txt
+rm apple.txt
 rm google.txt
 rm samsung.txt
 rm facebook.txt
@@ -126,18 +184,23 @@ clear
 echo "End of script..."
 echo
 echo
-echo "All rules for afwall were written to iptables_on.sh"
+echo "All rules for afwall were written to iptables_on.sh and iptables2_on.sh"
 echo
-echo "Now copy iptables_on.sh and iptables_off.sh to your internal storage."
+echo "Now copy iptables_on.sh, iptables2_on.sh and iptables_off.sh to your internal storage."
 echo
 echo "In AFWall press the three dots in the top right corner and select"
-echo "Set custom script. Now enter your path to the iptables_on.sh script"
+echo "Set custom script. Now enter your path to the iptables scripts"
 echo ". /[path to your storage]/iptables_on.sh" 
+echo ". /[path to your storage]/iptables2_on.sh"
+echo
+echo
+echo "NOTE: iptables2_on.sh is only generated if you chose Microsoft and/or Amazon ASN."
+echo
 echo
 echo "Do the same for custom shutdown script using iptables_off.sh"
 echo ". /[path to your storage]/iptables_off.sh"
 echo
-echo "For example: . /sdcard/iptables_on.sh"
+echo "For example: . /sdcard/iptables_off.sh"
 echo
 echo "IMPORTANT: Don't forget the ". /" point-space-slash before path!!"
 echo "##########################################################"
